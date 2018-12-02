575b0b4dbc88c4fd9494ff18d11803eafaafe4fa
ramsel <ramsel.batara@gmail.com>
2018-12-02 23:48:44 +0700

    second Commit

--- Exercise2.js
+++ Exercise2.js
@@ -1,21 +1,10 @@
-// function balikString(str){
-//     var word = "";
-//     for(var i = str[0].length-1 ; i>=0; i--){
-//         word+=str[0][i]
-//     }
-//     return word
-// }
+function balikString(str){
+    var word = "";
+    for(var i = str[0].length-1 ; i>=0; i--){
+        word+=str[0][i]
+    }
+    return word
+}
 
-// console.log(balikString(['hello world!']));
+console.log(balikString(['hello world!']));
 
-var int  = 12;
-var real = 3.45;
-var arr  = [6, 7, 8];
-
-var strInt  = String(int);
-var strReal = String(real);
-var strArr  = String(arr);
-
-console.log(strInt);  // '12'
-console.log(strReal); // '3.45'
-console.log(strArr);  // '6,7,8'
